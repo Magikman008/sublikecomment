aa498db964a2ccfab8ba2e1b9f854d10d85e6823
Magikman008 <40332480+Magikman008@users.noreply.github.com>
2020-05-07 23:10:56 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0) return;
+
+        if (low >= high) return;
+
+        int pivot = array[low + (high - low) / 2];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+
+            while (array[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j) quickSort(array, low, j);
+
+        if (high > i) quickSort(array, i, high);
+    }
+    public static void main(String[] args) {
+        int[] x = { 8, 0, 4, 7, 3, 6, 10, 12, -3 };
+        quickSort(x, 0, x.length - 1);
+        System.out.println(Arrays.toString(x));
+    }
+}
